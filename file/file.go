3059379c28463dac4b841a5aa1b6c2ce92672faa
Nick-nacher <spchkvvldsv@gmail.com>
2026-02-09 22:54:26 +0500

    ДЗ пакет os

--- file/file.go
+++ file/file.go
@@ -1,1 +1,20 @@
 package file
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func ReadFile(name string) (*[]byte, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+
+}
+
+func IsJson(path string) bool {
+	const ext string = ".json"
+	return filepath.Ext(path) == ext
+}
--- storage/storage.go
+++ storage/storage.go
@@ -1,1 +1,34 @@
 package storage
+
+import (
+	"3-struct/bins"
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadFile(name string) (*[]bins.Bin, error) {
+
+	data, err := os.ReadFile(name)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	var bin []bins.Bin
+	err = json.Unmarshal(data, &bin)
+	if err != nil {
+		return nil, err
+	}
+	return &bin, nil
+
+}
+
+func WriteFile(content []byte, name string) error {
+
+	err := os.WriteFile(name, content, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
